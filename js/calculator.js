795dee60a16bd606e0087d0048043b98d41729c6
david-gagne <dgagne813@gmail.com>
2018-05-16 10:53:32 -0400

    Added in the code to grab the selected payment processor from dropdown menu, and a Processor constructor with three processor instances.

---
+++ js/calculator.js
@@ -0,0 +1,27 @@
+let dropdownMenu = document.querySelector('body > div:nth-child(2) > div > div');
+let selectedDropdownMenuItem = document.querySelector('body > div:nth-child(2) > div > div > span.text');
+
+let paypal = new Processor('PayPal', .029, .30);
+let stripe = new Processor('Stripe', .029, .30);
+let square = new Processor('Square', .035, .15);
+
+function Processor(name, percentage, fixed) {
+  this.name = name;
+  this.percentage = Number(percentage);
+  this.fixed = Number(fixed);
+}
+
+function selected(processor) {
+  switch (processor.target.textContent) {
+    case "PayPal":
+    case "Stripe":
+      console.log(`This processor deducts 2.9% + 30¢`);
+      break;
+    case "Square":
+      console.log(`This processor deducts 3.5% + 15¢`);
+      break;
+    default:
+  }
+}
+
+dropdownMenu.addEventListener('click', selected);
