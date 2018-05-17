02a52a973327bb132c7bc9cacb6fbb9867902bb1
david-gagne <dgagne813@gmail.com>
2018-05-16 22:12:32 -0400

    Added in final piece of price-to-charge calculator method and set price to display in read-only result input field.

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,26 @@
 let dropdownMenu = document.querySelector('body > div:nth-child(2) > div > div');
 let selectedDropdownMenuItem = document.querySelector('body > div:nth-child(2) > div > div > span.text');
-
-let paypal = new Processor('PayPal', .029, .30);
-let stripe = new Processor('Stripe', .029, .30);
-let square = new Processor('Square', .035, .15);
+let targetRevenueInput = document.querySelector('body > div:nth-child(2) > div > span.ui.small.input.item > input[type="number"]');
+let resultInput = document.querySelector('body > div:nth-child(2) > div > span:nth-child(3) > input[type="text"]');
+let paypal = new Processor('PayPal', .971, .30);
+let stripe = new Processor('Stripe', .971, .30);
+let square = new Processor('Square', .965, .15);
 
 function Processor(name, percentage, fixed) {
   this.name = name;
-  this.percentage = Number(percentage);
+  this.yield = Number(percentage);
   this.fixed = Number(fixed);
   this.calculator = (targetRevenue) => {
-    console.log(`Hit the calculator method on the ${this.name} object`);
+    let price;
+    // console.log(`Hit the calculator method on the ${this.name} object`);
+    targetRevenue = Number(targetRevenueInput.value);
+    // console.log(`You want to earn ${targetRevenue}, right?`);
+    // N = (TR + PPfixed) / (1 - PP%)
+    price = (targetRevenue + this.fixed) / this.yield;
+    price = price.toFixed(2);
+    // console.log(price);
+    resultInput.value = price;
+
   }
 }
 
@@ -18,13 +28,19 @@ function Processor(name, percentage, fixed) {
 function selected(processor) {
   switch (processor.target.textContent) {
     case "PayPal":
+      // console.log(`This processor deducts 2.9% + 30¢`);
+      paypal.calculator();
+      break;
     case "Stripe":
-      console.log(`This processor deducts 2.9% + 30¢`);
+      // console.log(`This processor deducts 2.9% + 30¢`);
+      stripe.calculator();
       break;
     case "Square":
-      console.log(`This processor deducts 3.5% + 15¢`);
+      // console.log(`This processor deducts 3.5% + 15¢`);
+      square.calculator();
       break;
     default:
+    // Do nothing.
   }
 }
 
